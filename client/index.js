0640e326ed64dffee0c158c5990564ab3cc6b30a
anthonymarinated <anthonymarin718@gmail.com>
2022-07-13 17:12:29 -0400

    working app

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./styles.css";
+import "regenerator-runtime/runtime";
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
